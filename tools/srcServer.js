cfe55aed64bc699d77ea9d045f0e738e0ec5cb14
danbaumgart <danbaumgart@gmail.com>
2016-09-18 03:10:17 -0500

    fixes typo in webpack-dev-middleware inside ttools/srcServer.js (Express config) Prepares first npm script to start the app

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -10,7 +10,7 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
-app.use(require('wepback-dev-middleware')(compiler, {
+app.use(require('webpack-dev-middleware')(compiler, {
   noInfo:true,
   publicPath: config.output.publicPath
 }));
